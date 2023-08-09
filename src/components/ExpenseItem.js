c635c5c4ef35e873deec9a82898e0714daaf3f45
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-08-09 14:32:49 +0200

    refactor App to use dynamic ExpenseItem components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
+const ExpenseItem = ({ date, title, amount }) => {
   return (
     <div className="expense-item">
-      March 29th, 2023
+      <div>{date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
