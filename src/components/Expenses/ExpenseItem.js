d80106f0f33eed0317178361d33fa05565ee0c8b
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-08-11 18:08:57 +0200

    Refactor ExpenseItem and ExpenseForm with State

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = ({ date, title, amount }) => {
+  const [titleState, setTitleState] = useState(title);
+
+  const clickHandler = () => {
+    setTitleState("Updated");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{titleState}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
