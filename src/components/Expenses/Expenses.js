ef761ed8807b1b0bb590bf52ee6afc964df29f52
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-08-14 15:53:22 +0200

    Implement chart data visualization components

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = ({ expenses }) => {
   const [filteredYear, setFilteredYear] = useState("2023");
@@ -22,6 +23,7 @@ const Expenses = ({ expenses }) => {
           selectedYear={filteredYear}
           onChangeFilter={addFilterHandler}
         />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList expenses={filteredExpenses} />
       </Card>
     </div>
